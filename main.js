2131ff970d99880ae45d9c9f5e36d9fc095febbc
oussama-medfai <oussama-medfai>
2020-10-01 10:12:21 +0100

    1day

---
+++ main.js
@@ -0,0 +1,29 @@
+var users=[];
+function creatUser(user,pass){
+	return{
+		user:user,
+		pass:pass
+	}
+}
+users.push(creatUser('oussama','oussama'))
+users.push(creatUser('hiba','hiba'))
+users.push(creatUser('user','user'))
+users.push(creatUser('invit','invit'))
+
+
+
+function creatArticle(categorie) {
+	return{
+		categorie:categorie,
+		id="",
+		name:'',
+		url:"",
+		disciption:"",
+		price:"",
+		addarticle:addarticle,
+		deleteArticle:deleteArticle,
+		ModifieArticle:ModifieArticle,
+			};
+	
+}
+var article=[];
